80840ad80314d8d4b6f937d47019f612fe1cd2f6
Evan Willhite <evan@fourkitchens.com>
2019-06-17 11:13:03 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+const path = require("path")
+
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: path.join(__dirname, "src", "pages"),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-compile-es6-packages",
+      options: {
+        // replace with the name of your theme
+        modules: ["gatsby-theme-developer"],
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default () => <h1>Hello from gatsby-theme-developer</h1>
