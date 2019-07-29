fca6e2abdde07af460dbb717d3317701ba916747
Luke Herrington <lherrington100@gmail.com>
2019-07-29 13:39:23 -0700

    add TableOfContents for mdx (#33)
    * add TableOfContents for mdx
    
    * styling
    
    * Design tweaks

--- src/components/Templates/layout.js
+++ src/components/Templates/layout.js
@@ -8,14 +8,48 @@ import "./layout.css";
 // Components for MDX
 import ComponentViewer from "../Atoms/ComponentViewer/ComponentViewer.component";
 import CodeSnippet from "../Atoms/CodeSnippet/CodeSnippet.component";
+import TableOfContents from "../Molecules/TableOfContents/TableOfContents.component";
+
+const HEADINGS = [
+  ({ children, props }) => <h1 {...props}>{children}</h1>,
+  ({ children, props }) => <h2 {...props}>{children}</h2>,
+  ({ children, props }) => <h3 {...props}>{children}</h3>,
+  ({ children, props }) => <h4 {...props}>{children}</h4>,
+  ({ children, props }) => <h5 {...props}>{children}</h5>,
+  ({ children, props }) => <h6 {...props}>{children}</h6>
+];
+
+const Heading = level => ({ props, children }) => {
+  const H = HEADINGS[level - 1];
+  const name =
+    typeof children === "string"
+      ? children.replace(/\s+/g, "-").toLowerCase()
+      : "";
+  return (
+    <a name={name} href={`#${name}`}>
+      <H {...props}>{children}</H>
+    </a>
+  );
+};
 
 export default props => {
   const { pageContext } = props;
+  const post = props.data.mdx;
+
   /**
    * These components are exposed to the style guide authors
    * so that they can use MDX to layout their component documentation, usage, etc.
    */
   const [components] = React.useState({
+    h1: Heading(1),
+    h2: Heading(2),
+    h3: Heading(3),
+    h4: Heading(4),
+    h5: Heading(5),
+    h6: Heading(6),
+    TableOfContents: () => (
+      <TableOfContents items={post.tableOfContents.items} />
+    ),
     Component: props =>
       !pageContext.iframePath === null
         ? "Error: No Component Found"
@@ -31,7 +65,6 @@ export default props => {
         <CodeSnippet code={`${pageContext.twigCode}`} />
       )
   });
-  const post = props.data.mdx;
   const site = props.data.site;
   const docPages = props.data.allMdx.edges;
   const componentNodes = props.data.allFile.nodes;
@@ -78,6 +111,7 @@ export const pageQuery = graphql`
         description
         tab
       }
+      tableOfContents
       fields {
         parentDir
         collection
