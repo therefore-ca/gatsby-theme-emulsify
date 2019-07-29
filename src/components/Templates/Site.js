373927d4775f808b75b0770e8402bdb5dd88e5b7
Luke Herrington <lherrington100@gmail.com>
2019-07-29 11:23:26 -0700

    clean up Site component with hooks

--- src/components/Templates/Site.js
+++ src/components/Templates/Site.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
 import Sidebar from "../Organisms/Sidebar/Sidebar.component";
@@ -9,63 +8,52 @@ import "./site.css";
 import "./main.css";
 import "./main-design.css";
 
-export default class Site extends Component {
-  static propTypes = {
-    frontmatter: PropTypes.object.isRequired
+export default ({
+  title,
+  body,
+  docPages,
+  designSystems,
+  id,
+  menu,
+  parentDirectory,
+  collection
+}) => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleOpen = () => {
+    setIsMenuOpen(prevState => !prevState);
   };
 
-  state = { isMenuOpen: false };
-
-  toggleOpen = () => {
-    this.setState(prevState => ({
-      isMenuOpen: !prevState.isMenuOpen
-    }));
-  };
-
-  render() {
-    const {
-      title,
-      body,
-      docPages,
-      designSystems,
-      id,
-      menu,
-      parentDirectory,
-      collection
-    } = this.props;
-    let tabs = docPages
-      .filter(
-        docPage =>
-          docPage.node.fields &&
-          docPage.node.fields.parentDir === parentDirectory &&
-          docPage.node.frontmatter.tab
-      )
-      .sort(function(a, b) {
-        return a.node.frontmatter.tabOrder - b.node.frontmatter.tabOrder;
-      });
-    return (
-      <div
-        className={this.state.isMenuOpen ? "wrapper-open wrapper" : "wrapper"}
-      >
-        <div className="main">
-          <Sidebar
-            id={id}
-            pages={docPages}
-            siteTitle={title}
-            toggleOpen={this.props.toggleOpen}
-            menu={menu}
-            collection={collection}
-            designSystems={designSystems}
-          />
-          <div className="main-content">
-            <h1 className="main-title">{title}</h1>
-            {tabs.length ? <Tabs tabs={tabs} id={id} /> : null}
-            <div className="main-content-content">
-              <MDXRenderer>{body}</MDXRenderer>
-            </div>
+  let tabs = docPages
+    .filter(
+      docPage =>
+        docPage.node.fields &&
+        docPage.node.fields.parentDir === parentDirectory &&
+        docPage.node.frontmatter.tab
+    )
+    .sort(function(a, b) {
+      return a.node.frontmatter.tabOrder - b.node.frontmatter.tabOrder;
+    });
+  return (
+    <div className={isMenuOpen ? "wrapper-open wrapper" : "wrapper"}>
+      <div className="main">
+        <Sidebar
+          id={id}
+          pages={docPages}
+          siteTitle={title}
+          toggleOpen={toggleOpen}
+          menu={menu}
+          collection={collection}
+          designSystems={designSystems}
+        />
+        <div className="main-content">
+          <h1 className="main-title">{title}</h1>
+          {tabs.length ? <Tabs tabs={tabs} id={id} /> : null}
+          <div className="main-content-content">
+            <MDXRenderer>{body}</MDXRenderer>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
